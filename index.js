2022e93c68490416c8c6947d24261ed2b45d87a8
Kevin Digan <kd073@hotmail.com>
2021-09-20 22:05:07 -0400

    commit tree 7916383139d9a6f4e058da77be6c5267820c55d9

--- index.js
+++ index.js
@@ -1,2 +1,21 @@
 
 // Write your code here
+let num1 = 31;
+let num2 = 2;
+let multiply = num1 * num2;
+multiply;
+
+//let random = Math.random();
+//console.log(random);
+//Math.ceil((random) * 11);
+//console.log(Math.ceil);
+let random
+random = Math.floor(Math.random() * 10);
+
+let num3 = 19;
+let num4 = 5;
+let mod = (num3 % num4);
+mod;
+
+let max
+max = Math.max(5,10,15,20);
